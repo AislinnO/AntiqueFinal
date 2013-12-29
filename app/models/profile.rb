8af12838765eba844f9b8b4d92073e3c5c674017
Hanna-Mari Blankenstein <blankenstein.hannamari@gmail.com>
2013-12-29 22:18:02 +0000

    Added new tabs to navbar

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,6 +1,4 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
   has_many :orders
-  #attr_accessor :firstname, :lastname, :email, :address, :phone, :user_id
-  #attr_accessor :user_id
 end
